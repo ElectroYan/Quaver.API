a5cd3e590e6969bda1e33ddc1fd1d0c780ccf59a
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2020-05-13 15:18:24 +0200

    Add missing using

--- Quaver.API/Maps/Parsers/O2Jam/Ojn/OjnNoteChart.cs
+++ Quaver.API/Maps/Parsers/O2Jam/Ojn/OjnNoteChart.cs
@@ -1,6 +1,7 @@
 using Quaver.API.Maps.Parsers.O2Jam.EventPackages;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Quaver.API.Maps.Parsers.O2Jam
